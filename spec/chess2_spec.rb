0c695a2ac485478ce71dec98785b377f80f1215b
mpiriya <piriya.matt@gmail.com>
2021-06-13 17:38:31 -0400

    Began writing spec for new verison

---
+++ spec/chess2_spec.rb
@@ -0,0 +1,29 @@
+require "./lib/chess2.rb"
+
+describe Pawn do
+  describe possible_moves do
+    it "can move one square forward as long as there is no piece in front of it" do
+
+    end
+
+    it "can move two squares forward on first move" do
+
+    end
+    
+    it "takes diagonally" do
+
+    end
+
+    it "cannot move backwards" do 
+
+    end
+
+    it "cannot move if it puts its king in check" do
+
+    end
+  end
+end
+
+describe Knight do
+
+end
