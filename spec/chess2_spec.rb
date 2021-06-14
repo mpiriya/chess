6d4e26a8f722fbc6e7a0d2c4421191dc67df3a52
mpiriya <piriya.matt@gmail.com>
2021-06-13 23:26:15 -0400

    pawn tests

--- spec/chess2_spec.rb
+++ spec/chess2_spec.rb
@@ -3,159 +3,199 @@ require "./lib/chess2.rb"
 describe Pawn do
   describe "#possible_moves" do
     it "doesn't allow moving to the current square" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      b.set_piece(p.row, p.col, p)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 6 && m.to_cell.col == 6}).to eql(false)
     end
 
     it "allows Pawn to move one square forward" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      b.set_piece(p.row, p.col, p)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 5 && m.to_cell.col == 6}).to eql(true)
     end
 
     it "allows Pawn to move two squares forward on first move" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      b.set_piece(p.row, p.col, p)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 4 && m.to_cell.col == 6}).to eql(true)
     end
 
     it "doesn't allow Pawn to move on top of another piece" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      p2 = Pawn.new(b, "black", 5, 6)
+      b.set_piece(p.row, p.col, p)
+      b.set_piece(p2.row, p2.col, p2)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 5 && m.to_cell.col == 6}).to eql(false)
     end
 
     it "doesn't allow Pawn to jump over another piece" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      p2 = Pawn.new(b, "black", 5, 6)
+      b.set_piece(p.row, p.col, p)
+      b.set_piece(p2.row, p2.col, p2)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 4 && m.to_cell.col == 6}).to eql(false)
     end
     
     it "captures diagonally" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      p2 = Pawn.new(b, "black", 5, 5)
+      b.set_piece(p.row, p.col, p)
+      b.set_piece(p2.row, p2.col, p2)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 5 && m.to_cell.col == 5}).to eql(true)
     end
 
     it "doesn't capture allies" do
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      p2 = Pawn.new(b, "white", 5, 5)
+      b.set_piece(p.row, p.col, p)
+      b.set_piece(p2.row, p2.col, p2)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 5 && m.to_cell.col == 5}).to eql(false)
     end
 
     it "cannot move backwards" do 
-
+      b = Board.new
+      p = Pawn.new(b, "white", 6, 6)
+      b.set_piece(p.row, p.col, p)
+      poss = p.possible_moves
+      expect(poss.any? {|m| m.to_cell.row == 7 && m.to_cell.col == 6}).to eql(false)
     end
   end
 end
 
-describe Knight do
-  describe "#possible_moves" do
-    it "doesn't allow moving to the current square" do
+# describe Knight do
+#   describe "#possible_moves" do
+#     it "doesn't allow moving to the current square" do
 
-    end
+#     end
 
-    it "allows moving 2 vertically and 1 horizontally" do
+#     it "allows moving 2 vertically and 1 horizontally" do
       
-    end
+#     end
 
-    it "allows moving 1 vertically and 2 horizontally" do
+#     it "allows moving 1 vertically and 2 horizontally" do
       
-    end
+#     end
 
-    it "catches illegal moves" do
+#     it "catches illegal moves" do
       
-    end
+#     end
 
-    it "can capture enemies" do
+#     it "can capture enemies" do
       
-    end
-  end
-end
+#     end
+#   end
+# end
 
-describe Bishop do
-  describe "#possible_moves" do
-    it "doesn't allow moving to current square" do
+# describe Bishop do
+#   describe "#possible_moves" do
+#     it "doesn't allow moving to current square" do
       
-    end
+#     end
 
-    it "moves diagonally" do
+#     it "moves diagonally" do
       
-    end
+#     end
 
-    it "prevents non diagonal moves" do
+#     it "prevents non diagonal moves" do
       
-    end
+#     end
 
-    it "doesn't move through enemies" do
+#     it "doesn't move through enemies" do
       
-    end
+#     end
 
-    it "doesn't capture allies" do
+#     it "doesn't capture allies" do
       
-    end
+#     end
 
-    it "doesn't move through allies" do
+#     it "doesn't move through allies" do
       
-    end
+#     end
 
-    it "can capture enemies" do
+#     it "can capture enemies" do
       
-    end
-  end
-end
+#     end
+#   end
+# end
 
-describe Rook do
-  describe "#possible_moves" do
-    it "prevents moving nowhere" do
+# describe Rook do
+#   describe "#possible_moves" do
+#     it "prevents moving nowhere" do
       
-    end
+#     end
 
-    it "prevents moving rank AND file" do
+#     it "prevents moving rank AND file" do
       
-    end
+#     end
     
-    it "doesn't move through allies" do
+#     it "doesn't move through allies" do
       
-    end
+#     end
 
-    it "doesn't move through enemies" do
+#     it "doesn't move through enemies" do
       
-    end
+#     end
 
-    it "doesn't take allies" do
+#     it "doesn't take allies" do
       
-    end
+#     end
 
-    it "takes enemies" do
+#     it "takes enemies" do
       
-    end
-  end
-end
+#     end
+#   end
+# end
 
-describe Queen do
-  describe "#possible_moves" do
-    it "doesn't allow moving to current square" do
+# describe Queen do
+#   describe "#possible_moves" do
+#     it "doesn't allow moving to current square" do
       
-    end
+#     end
 
-    it "moves diagonally" do
+#     it "moves diagonally" do
       
-    end
+#     end
 
-    it "moves vertically" do
+#     it "moves vertically" do
 
-    end
+#     end
 
-    it "moves horizontally" do
+#     it "moves horizontally" do
       
-    end
+#     end
 
-    it "prevents illegal moves" do
+#     it "prevents illegal moves" do
       
-    end
+#     end
 
-    it "doesn't move through enemies" do
+#     it "doesn't move through enemies" do
       
-    end
+#     end
 
-    it "doesn't capture allies" do
+#     it "doesn't capture allies" do
       
-    end
+#     end
 
-    it "doesn't move through allies" do
+#     it "doesn't move through allies" do
       
-    end
+#     end
 
-    it "can capture enemies" do
+#     it "can capture enemies" do
       
-    end
-  end
-end
+#     end
+#   end
+# end
